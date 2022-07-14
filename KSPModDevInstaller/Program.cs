bb21cade5e6c333ca7baf784d653898f7faa8e8f
Standecco <dlombardo32@gmail.com>
2022-07-14 20:11:34 +0200

    Tweak csproj for actions

--- KSPModDevInstaller/Program.cs
+++ KSPModDevInstaller/Program.cs
@@ -177,7 +177,7 @@ namespace KSPModDevInstaller
             repoURL = reg.Match(answer).Groups[0].ToString();
             
             // make git clone the repo next to the running program
-            string? pwd = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string? pwd = Path.GetDirectoryName(Directory.GetCurrentDirectory());
             repoPath = $"{pwd}{DirSeparator}{repoName}";
             
             return AskYesOrNo($"Repo {repoName} found. It will be cloned at {repoPath}. Continue? ");
